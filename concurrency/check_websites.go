bcd8cca02615d5fbd3c4058914c53d9dc0aa4eef
xsadia <lipearthur@hotmail.com>
2022-01-17 21:08:35 -0300

    start concurrency chapter

---
+++ concurrency/check_websites.go
@@ -0,0 +1,19 @@
+package concurrency
+
+import "time"
+
+type WebsiteChecker func(string) bool
+
+func Checkwebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+
+	for _, url := range urls {
+		go func() {
+			results[url] = wc(url)
+		}()
+	}
+
+	time.Sleep(2 * time.Second)
+
+	return results
+}
---
+++ concurrency/check_websites_benchmark_test.go
@@ -0,0 +1,24 @@
+package concurrency
+
+import (
+	"testing"
+	"time"
+)
+
+func slowWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+
+	for i := 0; i < b.N; i++ {
+		Checkwebsites(slowWebsiteChecker, urls)
+	}
+}
