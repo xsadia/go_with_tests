f974717d33f482cd10ef26f851887cd6ec8b33cc
xsadia <lipearthur@hotmail.com>
2022-01-22 17:24:26 -0300

    wrapping concurrency chapter

--- concurrency/check_websites.go
+++ concurrency/check_websites.go
@@ -1,19 +1,26 @@
 package concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 func Checkwebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
-		go func() {
-			results[url] = wc(url)
-		}()
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
 	}
 
-	time.Sleep(2 * time.Second)
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+
+		results[r.string] = r.bool
+	}
 
 	return results
 }
